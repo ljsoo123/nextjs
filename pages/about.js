6835c217d27930b39a191a4d206d0f5d8deb7400
ljsoo123 <ljsoo123456@naver.com>
2021-07-02 19:03:12 +0900

    페이지 라우팅

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h2>hello</h2>
+    </div>
+  );
+};
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 const Index = () => {
   return (
     <>
       <div>index</div>
+      <h2>
+        <Link href="/about">
+          <a>소개</a>
+        </Link>
+      </h2>
     </>
   );
 };
