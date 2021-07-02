70c924c0473e135065056a77807610506e669f9e
ljsoo123 <ljsoo123456@naver.com>
2021-07-02 20:06:15 +0900

    title header

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 const Index = () => {
   return (
     <Layout>
+      <Head>
+        <title>Index 페이지</title>
+      </Head>
       <div>index</div>
       <h2>
         <Link href="/about">
